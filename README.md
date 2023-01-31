94c21c05705788aa601f13572dad2cf8882ba740
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-01-31 03:30:47 +0300

    firstThing(added css file and api with js)

--- README.md
+++
@@ -1,2 +0,0 @@
-# ASAC_final_project
-ASAC Final project
---
+++ js/booksApi.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('#searchForm');
+const inputFeild = document.querySelector('#searchID');
+
+async function getBookData(query){ 
+    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=search+terms${query}`);
+    let data = await response.json();
+    render(data);
+}
+
+function render(data){
+   let book = '';
+        for(let i = 0; i< data.items.length; i++){
+            const img = data.items[i].volumeInfo?.imageLinks?.smallThumbnail;
+            const title = data.items[i].volumeInfo.title;
+            const date = data.items[i].volumeInfo.publishedDate;
+            if(img && title && date){
+                book+=`
+                <div id="mainDiv">
+                <img class="bookImg" src=${img} />
+                <div class="bookTitle">${title}</div>
+                <div class="publishedDate">${date}</div>
+                </div>
+                `
+            }
+    }
+    console.log(book)
+    document.getElementById("resultContainer").innerHTML = book;
+}
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const feildValue = inputFeild.value;
+    getBookData(feildValue);
+  })
